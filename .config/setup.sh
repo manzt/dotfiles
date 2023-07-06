b03c3e4549691d7293c691534bd574f1440fea8d
Trevor Manz <trevor.j.manz@gmail.com>
2023-07-06 17:53:56 -0400

    feat: check-in setup script

---
+++ .config/setup.sh
@@ -0,0 +1,4 @@
+# Wipe all (default) app icons from the Dock
+# This is only really useful when setting up a new Mac, or if you don't use
+# the Dock to launch apps.
+defaults write com.apple.dock persistent-apps -array
