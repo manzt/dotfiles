1f9be6e9347fc80b7be777e28eadceae34b39d2b
Trevor Manz <trevor.j.manz@gmail.com>
2025-04-28 15:53:41 -0400

    Update learn-lsp path

--- .config/nvim/lua/learn-lsp.lua
+++ .config/nvim/lua/learn-lsp.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.setup = function()
   vim.lsp.config("learn_lsp", {
-    cmd = { 'deno', 'run', '-A', '/Users/manzt/demos/learn-lsp/server/main.ts' },
+    cmd = { 'deno', 'run', '-A', '/Users/manzt/github/manzt/learn-lsp/server/main.ts' },
     filetypes = { 'markdown' },
     single_file_support = true,
   });
