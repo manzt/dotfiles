65665b9da73846c2bebdc9088c2cbc4b77ec4d75
Trevor Manz <trevor.j.manz@gmail.com>
2025-07-21 10:01:26 -0400

    Add `toppair/peek.vim` for markdown preview

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -465,6 +465,16 @@ require("lazy").setup({
     },
     keys = { { "-", ":Oil<CR>" } }
   },
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
 })
 
 -- Work around to get Deno virtual LSP locations working
